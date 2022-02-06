4af0b7480b47b77f8ec5b6ffe99aaeac430ac8d1
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2022-02-06 14:17:36 +0900

    fix: locustfile

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -7,16 +7,29 @@ class LaravelLoadTestUser(HttpUser):
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
+    login_token = ""
 
-    @task
-    def index(self):
-        self.client.get("/api")
+    def authHeaders(self):
+        return {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + self.login_token
+        }
 
     @task
     def register(self):
-        self.client.post("/api/register", json={
-            "name":"example",
-            "email":"email" + str(uuid.uuid4()) + "@example.test",
+        self.client.get("/", headers = self.headers)
+
+        with self.client.post("/register", json={
+            "name": "example",
+            "email": str(uuid.uuid4()) + "@laravel-load.test",
+            "password": "secretsecret"
+        }, headers = self.headers, catch_response=True) as response:
+            self.login_token = response.json()["login_token"]
+
+        self.client.post("/login", json={
             "password": "secretsecret",
-            "password_confirmation": "secretsecret"
+            "login_token": self.login_token
         }, headers = self.headers)
+
+        self.client.get("/user", json={}, headers = self.authHeaders())
