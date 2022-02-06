2e53858385192cb7d2305cb35150e92f769b9e8d
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2022-02-06 16:08:08 +0900

    Runtime templates

--- docker/mysql/init-db.sql
+++ docker/mysql/init-db.sql
@@ -1,8 +1,7 @@
-CREATE DATABASE IF NOT EXISTS `laravel_prefork`;
-CREATE DATABASE IF NOT EXISTS `laravel_event`;
-CREATE DATABASE IF NOT EXISTS `laravel_nginx`;
-CREATE DATABASE IF NOT EXISTS `laravel_nginxunit`;
-CREATE DATABASE IF NOT EXISTS `laravel_roadrunner`;
+CREATE DATABASE IF NOT EXISTS `laravel-01-apache`;
+CREATE DATABASE IF NOT EXISTS `laravel-02-fpm`;
+CREATE DATABASE IF NOT EXISTS `laravel-03-nginxunit`;
+CREATE DATABASE IF NOT EXISTS `laravel-04-rr`;
 
 CREATE USER 'laravel-writer'@'localhost' IDENTIFIED BY 'laravel-writer';
 CREATE USER 'laravel-writer'@'%' IDENTIFIED BY 'laravel-writer';
